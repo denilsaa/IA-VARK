f7ff37462055366090857cee92378166e3a75c70
Denilson Saavedra <denilsonsaavedra2005@gmail.com>
2026-06-02 00:07:57 -0400

    Ejecutar collectstatic en Render

--- Dockerfile
+++ Dockerfile
@@ -20,3 +20,7 @@ COPY . .
 EXPOSE 8000
 
 CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
+
+RUN python manage.py collectstatic --noinput
+
+CMD ["./start.sh"]
---
+++ start.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -o errexit
+
+python manage.py migrate --noinput
+python manage.py collectstatic --noinput
+
+gunicorn config.wsgi:application --bind 0.0.0.0:${PORT:-10000} --workers ${WEB_CONCURRENCY:-2} --timeout ${GUNICORN_TIMEOUT:-180}
